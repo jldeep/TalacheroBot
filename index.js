c16766dd870a80f95af07995ece4b8ed35267091
José Luis Zenteno De León <jldeep@icloud.com>
2015-12-19 20:33:31 -0600

    Added Google Image search

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const conf = require('./conf')
 const TelegramBot = require('node-telegram-bot-api')
 const Youtube = require('./lib/youtube')
+const googleImages = require('./lib/google-images')
+const fs = require('fs')
 
 let token = conf.telegram
 
@@ -25,5 +27,36 @@ bot.onText(/\/yt/, msg => {
   .catch(error => {
     let message = 'Hubo un problema desconocido :('
     bot.sendMessage(chatId, message)
+  })
+})
+
+bot.onText(/\/img/, msg => {
+  let chatId = msg.chat.id
+  let message = msg.text.split("/img ")
+  let position = -1
+  let search = ''
+  if (message[1].indexOf('%') > -1) {
+    let text = message[1].split("% ")
+    position = text[0]
+    search = text[1]
+  }
+  else {
+    search = message[1]
+  }
+  let opts = {
+    reply_to_message_id: msg.message_id
+  }
+  googleImages(position, search)
+  .then(image => {
+    return [bot.sendPhoto(chatId, image), image]
+  })
+  .spread((resp, image) => {
+    fs.unlinkSync(image)
+  })
+  .catch(err => {
+    bot.sendMessage(chatId, msg.from.first_name + 'la imagen presentó un problema. Intentalo de nuevo :(' , opts);
+  })
+  .catch(err => {
+    bot.sendMessage(chatId, msg.from.first_name + ' ' + err.message , opts);
   })
 })
